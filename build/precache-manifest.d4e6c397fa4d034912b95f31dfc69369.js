e860554cf35623d24fd8a894e793f27248bc6cae
naman0105 <naman0105@gmail.com>
2018-12-01 14:41:12 +0530

    front-end in build

---
+++ build/precache-manifest.d4e6c397fa4d034912b95f31dfc69369.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "045c84d98c95663f7683",
+    "url": "/static/js/main.045c84d9.chunk.js"
+  },
+  {
+    "revision": "28d3dbe086713ec61895",
+    "url": "/static/js/1.28d3dbe0.chunk.js"
+  },
+  {
+    "revision": "045c84d98c95663f7683",
+    "url": "/static/css/main.7f9f716e.chunk.css"
+  },
+  {
+    "revision": "28d3dbe086713ec61895",
+    "url": "/static/css/1.c7a8018d.chunk.css"
+  },
+  {
+    "revision": "21e1f691fa2ecee01f1fe27aad8de4ae",
+    "url": "/index.html"
+  }
+];
